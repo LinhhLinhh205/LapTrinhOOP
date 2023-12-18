6f49a7e463ba30502eadc5546a03d91f75355969
NgocLinh <linhlinhbehy@gmail.com>
2023-12-18 11:19:54 +0700

    Code phien ban 2

--- chapter1/chapter1/demo1/Program.cs
+++ chapter1/chapter1/demo1/Program.cs
@@ -11,6 +11,7 @@ namespace demo1
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+            Console.WriteLine("chao ban");
             Console.ReadLine();
         }
     }
